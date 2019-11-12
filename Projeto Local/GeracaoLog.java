9334c9508a2fc70f07b27d297e33474122baafee
EsterPaixao <ester.aquino.ea23@gmail.com>
2019-11-12 16:38:16 -0200

    Log txt

---
+++ Projeto Local/GeracaoLog.java
@@ -0,0 +1,31 @@
+package model;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeracaoLog {
+    
+    public static void GravarLog(String mensagem) throws IOException {
+        
+        String pasta = "C:\\LogsNextStreaming";
+        
+        File diretorio = new File(pasta);
+            diretorio.mkdir();
+        File arquivo = new File(pasta + "\\LogsdeExecução.txt");
+
+        if (!arquivo.exists()) {
+            arquivo.createNewFile();
+        }
+        List<String> lista = new ArrayList<>();
+        lista.add("[" + LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)) + "] " + mensagem);
+        Files.write(Paths.get(arquivo.getPath()), lista, StandardOpenOption.APPEND);
+    }
+}
